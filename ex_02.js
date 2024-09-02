b00ef380d1857739291c604dc6e25c94acd47308
Hanane6 <hanane.ben-ahmed@epitech.digital>
2024-09-02 10:53:27 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,14 @@
+// The module path to fiddles accepts all the common variations:
+//
+// - @jsfiddle/[fiddle].js
+// - @jsfiddle/[user]/[fiddle].js
+// - @jsfiddle/[user]/[fiddle]/[version].js
+// - @jsfiddle/[user]/[fiddle]/latest.js
+//
+// You need to remember to add `.js` at the end.
 
+import FiddleModule from "@jsfiddle/rLd4uyep.js"
+
+let message = FiddleModule.greet("Hello world")
+
+document.querySelector("p").innerHTML = message
